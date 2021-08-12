e5f0e8882fee16a6f0dc4a2d59b1810975e2bd5a
USPRO <usdogu@gmail.com>
2021-08-12 23:12:31 +0300

    add installer

---
+++ install.ps1
@@ -0,0 +1,6 @@
+$exe = Read-Host -Prompt "Exe Path: "
+$content = Get-Content -Path ".\integrate-shell.reg"
+$exe = $exe -replace "\\", "\\"
+$exe = $exe -replace '"',""
+$content = $content -replace "REPLACETHIS",$exe
+$content | Set-Content -Path ".\integrate-shell.reg"
