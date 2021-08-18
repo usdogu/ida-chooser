6b4228ca246383ef09370d272a826a4edcfa9956
USPRO <usdogu@gmail.com>
2021-08-18 15:59:40 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/usdogu/ida-chooser
+
+go 1.16
+
+require golang.org/x/sys v0.0.0-20210817190340-bfb29a6856f2
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	if IsFirstRun() {
+		fmt.Print("Path to IDA folder: ")
+		var path string
+		fmt.Scanln(&path)
+		path = strings.TrimSpace(path)
+		SetIDAPath(path)
+	}
+
+	ida_path := GetIDAPath()
+
+	if GetArch() == 0x8664 {
+		ida_path += `\ida64.exe `
+	} else {
+		ida_path += `\ida.exe `
+	}
+	exec.Command(ida_path, os.Args[1]).Output()
+
+	//os.Exit(0)
+}
